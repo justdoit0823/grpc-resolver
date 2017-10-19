05d6e86f6067389ae1e8835f501e5db22d3e9109
余森彬 <yusenbin@laiye.com>
2017-10-19 17:45:25 +0800

    Fix wrong resolve action.

--- grpcresolver/resolver.py
+++ grpcresolver/resolver.py
@@ -83,7 +83,7 @@ class EtcdServiceResolver(ServiceResolver):
         keys = self._client.get_prefix(name)
         vals = []
         plain = True
-        if isinstance(self._addr_cls, PlainAddress):
+        if not isinstance(self._addr_cls, PlainAddress):
             plain = False
 
         for val, metadata in keys:
